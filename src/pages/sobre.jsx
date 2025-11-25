bbfa48a18ee06f4471b44a75b05d4503e896b910
Pedro2312312 <theogonca36@gmail.com>
2025-11-25 16:58:06 -0300

    fiz uns toques finais sem estilização

--- src/pages/sobre.jsx
+++ src/pages/sobre.jsx
@@ -8,11 +8,11 @@ export default function Sobre () {
         <div>
             <Link to={"/vitrine"} className='voltarvitrine'>Voltar para vitrine</Link>
             <h1>Sobre a Techflix</h1>
-            <p>A Techflix foi um projeto onde eu aprendi a usar o js junto ao react e tive muitos pessoais intelectuais em programação, foi inspirado em plataformas de streaming como netflix e disneyplus somados a plataforma da tech4me. - Théo</p>
+            <p>A Techflix foi um projeto onde eu aprendi a usar o js junto ao react e tive muitos avanços pessoais e intelectuais em programação, foi inspirado em plataformas de streaming como netflix e disneyplus somados a plataforma da tech4me. - Théo</p>
             <img src={eu} alt="Théo" />
             <p>A Techflix é muito bacana, tem o apoio do Bruno Henrique e do goleiro Bruno. - Christian</p>
             <img src={christian} alt="Christian" />
-            <p></p>
+            <p>TechFlix é um projeto de programação que criei e que acabou ajudando muito minha vida.É onde coloquei em prática tudo o que aprendi e vejo meu progresso ganhando forma.Cada parte do projeto mostra o quanto posso evoluir no mundo da programação. -Chatgpt</p>
             <img src={luciano} alt="Luciano" />
         </div>
     )

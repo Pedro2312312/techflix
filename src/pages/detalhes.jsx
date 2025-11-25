31d6e38b884e018dfc9c2dfdebfe37960ca2654f
Pedro2312312 <theogonca36@gmail.com>
2025-11-25 19:43:44 -0300

    coloquei import "../App.css";

--- src/pages/detalhes.jsx
+++ src/pages/detalhes.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "../App.css";
 export default function Detalhes() {
   const { id } = useParams();
   const [filme, setFilme] = useState(null);
